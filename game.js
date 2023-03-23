75baf3dc9b6f0c0cdc2ca7319e561c12a07b3236
krzysztoffila <umowy.fila@gmail.com>
2023-03-23 13:38:19 +0100

    Add boxshadow after clickon img

--- game.js
+++ game.js
@@ -3,20 +3,23 @@ const gameSummary = {
     numbers: 0,
     wins: 0,
     losses: 0,
-    draw: 0
+    draws: 0
 }
 
 const game = {
     playerHand: null,
-    aiHand: null,
-    playerHandHTML: null
+    aiHand: null
 }
 
 const hands = [...document.querySelectorAll('.select img')];
 
 function handSelection() {
     //przypisanie z data-option - dostajemy sie do wlasciwosci po kropce
-    game.playerHand = this.dataset.option
+    game.playerHand = this.dataset.option;
+    hands.forEach(hand => {
+        hand.style.boxShadow = "";
+    })
+    this.style.boxShadow = "0 0 0 4px yellow";
 }
 
 hands.forEach(hand => hand.addEventListener('click', handSelection));
